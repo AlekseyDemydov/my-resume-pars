71a7fc7a161ffbc37d7889947fa8f2ea330c2b4a
AlekseyDemydov <96746949+AlekseyDemydov@users.noreply.github.com>
2022-08-07 18:57:12 +0300

    go

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,7 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
   const projectItem = document.querySelectorAll('.res-content-project-item');
+  const photo = document.querySelector('.photo');
 
-  const scrollAnimation = () => {
+  const scrollAnimationPhoto = () => {
+    let scrollPh = window.scrollY;
+    console.log(scrollPh);
+    let photoCenter = 0;
+    console.log(photoCenter);
+    if (scrollPh === photoCenter) {
+      photo.classList.add('active');
+    } else {
+      photo.classList.remove('active');
+    }
+  };
+  const scrollAnimationProject = () => {
     let center = window.innerHeight / 4;
     let projectCenter = center + window.scrollY;
     projectItem.forEach(el => {
@@ -13,8 +25,9 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     });
   };
-  scrollAnimation();
+
   window.addEventListener('scroll', () => {
-    scrollAnimation();
+    scrollAnimationProject();
+    scrollAnimationPhoto();
   });
 });
