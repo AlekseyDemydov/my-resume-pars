207b5dbc5255d20ae5aa3e56d6c0982556414d12
AlekseyDemydov <agdemidof@gmail.com>
2022-09-01 23:52:13 +0300

    fix

--- src/js/lang.js
+++ src/js/lang.js
@@ -5,86 +5,106 @@ const langArr = {
   },
   "one": {
     "en": "Ability to work efficiently both individually and in a team",
-    "ua":"Вміння ефективно працювати як індивідуально, так і в команді"
+    "ua": "Вміння ефективно працювати як індивідуально, так і в команді",
   },
   "two": {
     "en": "Creative thinking skills",
-    "ua":"Творче мислення"
+    "ua": "Творче мислення",
   },
   "thre": {
     "en": "Organizational skills",
-    "ua":"Організаційні навички"
+    "ua": "Організаційні навички",
   },
   "four": {
     "en": "Willingness to learn",
-    "ua":"Готовність вчитися"
+    "ua": "Готовність вчитися",
   },
-   "contacts": {
+  "five": {
+    "en": "Teamwork",
+    "ua": "Командний гравець",
+  },
+  "contacts": {
     "en": "Contacts",
-    "ua":"Контакти"
+    "ua": "Контакти",
   },
-   "tel": {
+  "tel": {
     "en": "tel:&nbsp",
-    "ua":"тел:&nbsp"
+    "ua": "тел:&nbsp",
   },
-   "mail": {
+  "mail": {
     "en": "mail:&nbsp",
-    "ua":"пошта:&nbsp"
+    "ua": "пошта:&nbsp",
   },
   "soft": {
     "en": "Soft skills",
-    "ua":"Особисті якості"
+    "ua": "Особисті якості",
   },
   "tech": {
     "en": "Tech skills",
-    "ua":"Технічні навички"
+    "ua": "Технічні навички",
   },
   "educ": {
     "en": "Education",
-    "ua":"Освіта"
+    "ua": "Освіта",
   },
   "devel": {
     "en": "Developer",
-    "ua":"Розробник"
+    "ua": "Розробник",
   },
   "develop": {
     "en": "Developer",
-    "ua":"Розробник"
+    "ua": "Розробник",
   },
   "summ": {
     "en": "Summary",
-    "ua":"Резюме"
+    "ua": "Резюме",
   },
   "summText": {
     "en": "I am a front-end developer looking for a full-time job in company. I have good knowledge of HTML, CSS and JavaScript. In addition, I have practical experience of working as a team leader. I learn fast, responsible and ready for complex tasks.",
-    "ua":"Я фронтенд розробник, шукаю роботу на повний робочий день у компанії. Я добре знаю HTML, CSS та JavaScript. Крім того, маю практичний досвід роботи керівником групи. Навчаюсь швидко, відповідальний та готовий до складних завдань."
+    "ua": "Я фронтенд розробник, шукаю роботу на повний робочий день у компанії. Я добре знаю HTML, CSS та JavaScript. Крім того, маю практичний досвід роботи керівником групи. Навчаюсь швидко, відповідальний та готовий до складних завдань.",
   },
   "proj": {
     "en": "Project experience:",
-    "ua":"Портфоліо:"
+    "ua": "Портфоліо:",
   },
   "btnSendMe": {
     "en": "send me",
-    "ua":"Написати мені"
+    "ua": "Написати мені",
   },
   "follow": {
     "en": "follow me",
-    "ua":"Зв'язатися зі мною"
+    "ua": "Зв'язатися зі мною",
   },
   "btnSend": {
     "en": "send",
-    "ua":"Відправити"
+    "ua": "Відправити",
   },
   "date": {
     "en": "Apr 2022- present",
-    "ua":"Квітень 2022- дотепер"
+    "ua": "Квітень 2022- дотепер",
   },
   "vuz": {
     "en": "T.H. SHEVCHENKO NATIONAL UNIVERSITY CHERNIHIV COLEHIUM (NUCHK)",
-    "ua":"Національний університет Чернігівський колегіум імені Т.Г. Шевченка"
+    "ua": "Національний університет Чернігівський колегіум імені Т.Г. Шевченка",
   },
   "fak": {
     "en": "Faculty of Physics and Mathematics, specialty: mathematics and informatics | Sep 2019 - present",
-    "ua":"Фізико-математичний факультет, спеціальність: математика-інформатика | Вересень 2019 - дотепер"
+    "ua": "Фізико-математичний факультет, спеціальність: математика-інформатика | Вересень 2019 - дотепер",
+  },
+  "country": {
+    "en": "Chernihiv, Ukraine",
+    "ua": "Чернігів, Україна",
+  },
+  "eng": {
+    "en": "English - Pre-Intermediate",
+    "ua": "Англійська - Нижче за середній",
+  },
+  "ukr": {
+    "en": "Ukrainian - Native",
+    "ua": "Українська - Рідна",
+  },
+  "rus": {
+    "en": "Russian - Advanced",
+    "ua": "Російська - Просунутий",
   },
 };
--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,18 +1,20 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const projectItem = document.querySelectorAll('.item');
-  const photo = document.querySelector('.photo');
-  const contacts = document.querySelector('.contacts');
-  const techSkills = document.querySelector('.tech-skills');
-  const softSkills = document.querySelector('.soft-skills');
+document.addEventListener("DOMContentLoaded", () => {
+  const projectItem = document.querySelectorAll(".item");
+  const photo = document.querySelector(".photo");
+  const contacts = document.querySelector(".contacts");
+  const techSkills = document.querySelector(".tech-skills");
+  const softSkills = document.querySelector(".soft-skills");
+  const language = document.querySelector(".languages");
 
   let photoCenter = photo.offsetHeight;
   let scrollPh = window.scrollY + photoCenter;
 
   if (scrollPh === photoCenter) {
-    photo.classList.add('activeLeft');
-    contacts.classList.add('activeRight');
-    techSkills.classList.add('activeLeft');
-    softSkills.classList.add('activeRight');
+    photo.classList.add("activeLeft");
+    contacts.classList.add("activeRight");
+    techSkills.classList.add("activeLeft");
+    softSkills.classList.add("activeRight");
+    language.classList.add("activeLeft");
   }
   const scrollAnimationPhoto = () => {
     let centerWind = window.scrollY;
@@ -20,23 +22,25 @@ document.addEventListener('DOMContentLoaded', () => {
     let scrollPh = (centerWind + techSkillsCenter) / 5;
 
     if (scrollPh <= techSkillsCenter) {
-      photo.classList.add('activeLeft');
-      contacts.classList.add('activeRight');
-      techSkills.classList.add('activeLeft');
+      photo.classList.add("activeLeft");
+      contacts.classList.add("activeRight");
+      techSkills.classList.add("activeLeft");
+      language.classList.add("activeLeft");
     } else {
-      photo.classList.remove('activeLeft');
-      contacts.classList.remove('activeRight');
-      techSkills.classList.remove('activeLeft');
+      photo.classList.remove("activeLeft");
+      contacts.classList.remove("activeRight");
+      techSkills.classList.remove("activeLeft");
+      language.classList.remove("activeLeft");
     }
     let softSkilsCenter = softSkills.offsetHeight;
-   
+
     let scrollSoft = centerWind + softSkilsCenter;
-    
+
     if (scrollSoft > softSkilsCenter) {
-      softSkills.classList.add('activeRight');
+      softSkills.classList.add("activeRight");
     }
     if (scrollSoft > 1000) {
-      softSkills.classList.remove('activeRight');
+      softSkills.classList.remove("activeRight");
     }
   };
 
@@ -45,7 +49,7 @@ document.addEventListener('DOMContentLoaded', () => {
   projectItem.forEach(el => {
     let scrollTop = el.offsetTop + el.offsetHeight;
     if (projectCenter > scrollTop) {
-      el.classList.add('animation');
+      el.classList.add("animation");
     }
   });
   const scrollAnimationProject = () => {
@@ -54,14 +58,14 @@ document.addEventListener('DOMContentLoaded', () => {
     projectItem.forEach(el => {
       let scrollTop = el.offsetTop + el.offsetHeight;
       if (projectCenter > scrollTop) {
-        el.classList.add('animation');
+        el.classList.add("animation");
       } else {
-        el.classList.remove('animation');
+        el.classList.remove("animation");
       }
     });
   };
 
-  window.addEventListener('scroll', () => {
+  window.addEventListener("scroll", () => {
     scrollAnimationProject();
     scrollAnimationPhoto();
   });
